2837cbf0d51fdf3e463018098253c4a3e228cc88
Jacob Roe <jroenavy24@gmail.com>
2021-06-07 14:32:15 -0700

    commit

--- PythagoreanTheoremTest.java
+++ PythagoreanTheoremTest.java
@@ -2,7 +2,7 @@ public class PythagoreanTheoremTest {
     public static void main(String[] args){
 
         PythagoreanTheorem p = new PythagoreanTheorem();
-        double someNum = p.calculateHypotenuse(1,1);
+        double someNum = p.calculateHypotenuse(3,5);
         System.out.println(someNum);
     }   
 }
